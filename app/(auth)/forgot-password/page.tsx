04ede0b4a3eedca246123b434fe825ae712258af
samina <afrozsamina17@gmail.com>
2025-11-12 02:00:47 +0600

    Update project with latest changes

---
+++ app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,11 @@
+// app/(auth)/forgot-password/page.tsx
+import React from "react";
+
+export default function ForgotPasswordPage() {
+  return (
+    <div>
+      <h1>Forgot Password</h1>
+      {/* Your form or content here */}
+    </div>
+  );
+}
